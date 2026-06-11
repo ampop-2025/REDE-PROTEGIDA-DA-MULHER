450fff7f0c4e8e6e88614805857ac2b699bfeedd
ampop-2025 <josineiamaral@gmail.com>
2026-06-11 18:35:03 -0300

    Update auth.js

--- assets/js/auth/auth.js
+++ assets/js/auth/auth.js
@@ -1,7 +1,20 @@
-import { signInWithEmailAndPassword }
+import {
+signInWithEmailAndPassword
+}
 from "https://www.gstatic.com/firebasejs/12.2.1/firebase-auth.js";
 
-import { auth }
+import {
+collection,
+query,
+where,
+getDocs
+}
+from "https://www.gstatic.com/firebasejs/12.2.1/firebase-firestore.js";
+
+import {
+auth,
+db
+}
 from "../firebase/firebase.js";
 
 window.login = async function(){
@@ -12,6 +25,11 @@ window.login = async function(){
     const senha =
     document.getElementById("senha").value;
 
+    const msg =
+    document.getElementById("msg");
+
+    msg.innerHTML = "";
+
     try{
 
         await signInWithEmailAndPassword(
@@ -20,14 +38,75 @@ window.login = async function(){
             senha
         );
 
-        window.location.href =
-        "dashboard.html";
+        const q = query(
+            collection(db,"usuariosSistema"),
+            where("email","==",email)
+        );
+
+        const snapshot =
+        await getDocs(q);
+
+        if(snapshot.empty){
+
+            msg.innerHTML =
+            "Usuário não cadastrado no sistema.";
+
+            return;
+        }
+
+        const usuario =
+        snapshot.docs[0].data();
+
+        if(usuario.ativo !== true){
+
+            msg.innerHTML =
+            "Usuário inativo.";
+
+            return;
+        }
+
+        switch(usuario.tipo){
+
+            case "admin":
+
+                window.location.href =
+                "pages/admin/dashboard.html";
+
+                break;
+
+            case "operador":
+
+                window.location.href =
+                "pages/operador/dashboard.html";
+
+                break;
+
+            case "contato":
+
+                window.location.href =
+                "pages/contato/dashboard.html";
+
+                break;
+
+            case "usuario":
+
+                window.location.href =
+                "pages/usuario/dashboard.html";
+
+                break;
+
+            default:
+
+                msg.innerHTML =
+                "Perfil inválido.";
+
+        }
 
     }
     catch(error){
 
-        document.getElementById("msg")
-        .innerHTML = error.message;
+        msg.innerHTML =
+        error.message;
 
     }
 
