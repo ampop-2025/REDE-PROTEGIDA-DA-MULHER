56be68f1946955a0b7a80d9c7e01a6d38da2b420
ampop-2025 <josineiamaral@gmail.com>
2026-06-11 16:04:04 -0300

    Update auth.js

--- assets/js/auth/auth.js
+++ assets/js/auth/auth.js
@@ -1,1 +1,34 @@
+import { signInWithEmailAndPassword }
+from "https://www.gstatic.com/firebasejs/12.2.1/firebase-auth.js";
 
+import { auth }
+from "./firebase.js";
+
+window.login = async function(){
+
+    const email =
+    document.getElementById("email").value;
+
+    const senha =
+    document.getElementById("senha").value;
+
+    try{
+
+        await signInWithEmailAndPassword(
+            auth,
+            email,
+            senha
+        );
+
+        window.location.href =
+        "dashboard.html";
+
+    }
+    catch(error){
+
+        document.getElementById("msg")
+        .innerHTML = error.message;
+
+    }
+
+}
